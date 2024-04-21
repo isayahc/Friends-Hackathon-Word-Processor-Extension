9af1b7de815fff5069e3c3bc905f7c8f9d84a6cd
isayhc <isayahculbertson@gmail.com>
2024-04-21 10:37:59 -0400

    removed conversational history and prompted the agent to better format citations

--- react_json_with_memory.py
+++ react_json_with_memory.py
@@ -1,5 +1,6 @@
 template_system = """
-Answer the following questions as best you can. You have access to the following tools:
+You will be given a fact and you must search if it is true or not. 
+If True provide a source in APA format and ONLY return the source. Else just return "False" You have access to the following tools:
 
 <TOOLS>
 {tools}
@@ -21,7 +22,9 @@ The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of mult
 
 ALWAYS use the following format:
 
-Question: you will be given a fact. You must find the source of this fact. Please structure the reponse as APA format
+Fact: you will be given a fact. You must find the source of this fact. Please structure the reponse as APA format. 
+Only return the source in the response unless the fact is false If the fact is false then return false.
+
 Thought: you should always think about what to do
 Action:
 ```
@@ -30,14 +33,17 @@ $JSON_BLOB
 Observation: the result of the action
 ... (this Thought/Action/Observation can repeat N times)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+Final Answer: The APA source of the fact if the fact is true
+
+the repsonse should look like one of the following:
+
+Author, A. A., Author, B. B., & Author, C. C. (Year). Title of the article. Journal Name, Volume(Issue), Page range. DOI or URLExample:Smith, J. D., Johnson, R. S., & Williams, L. M. (2023). The effects of mindfulness meditation on stress reduction. Journal of Applied Psychology, 45(2), 123-135. https://doi.org/10.1037/apl0000123
+Author, A. A. (Year). Title of the book. Publisher.Example:Jones, S. M. (2018). Understanding Psychology: An Introduction. Oxford University Press.
+Author, A. A. (Year). Title of the webpage. Website Name. URLExample:National Institute of Mental Health. (2022). Depression. National Institute of Mental Health. https://www.nimh.nih.gov/health/topics/depression/index.shtml
 
 Begin! Reminder to always use the exact characters `Final Answer` when responding.
 
 Previous conversation history:
-<CONVERSATION_HISTORY>
-{chat_history}
-</CONVERSATION_HISTORY>
 
 <NEW_INPUT>
 {input}
